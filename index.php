6e7af8d7e75de8433f5c1c02b4c819535d7b7d25
Domestos04 <klimenkoantonkhua@gmail.com>
2022-09-22 13:12:27 +0300

    3.7 therm

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+print "<table style=\"border:solid;\">";
+for ($i = -20; $i < 21; $i++) {
+    
+    print "<tr><td style=\"border:solid;\">$i</td><td style=\"border:solid;\" width=\"30\" height=\"150\"></td></tr>";
+}
+print "</table>";
