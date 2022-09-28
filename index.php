64d29925ca32895e69f7178b6341db4db2441647
Domestos04 <klimenkoantonkhua@gmail.com>
2022-09-28 15:06:15 +0300

    ..

--- index.php
+++ index.php
@@ -2,12 +2,17 @@
 print "<table style=\"border:solid;\">";
 for ($i = 20; $i > -21; $i--) {
     $t = rand(-20, 20);
-    if($t > $i);
-    else($t < $i);
-    echo ($style = ($t > $i));
-    echo ($style = ($t < $i));
+    if($t > $i){
+        $style = 'width:30px;background:red';
+    }
+    else{
+        $style = 'width:30px;background:yellow';
+    }
+    //echo ($style = ($t > $i));
     
-    print "<tr><td style=\"border:solid;\">$i</td><td style=\"border:solid;\" width=\"30\" height=\"100\" style=\"{$style}\" width=\"30\" \"background:red;\" \"width:30\" \"background:yellow;\"></td></tr>";
+    //echo ($style = ($t < $i));
+    
+    print "<tr><td style=\"border:solid;\">$i</td><td style=\"border:solid;\" width=\"30\" height=\"50\" \"{$style}\"></td></tr>";
     
 }
 print "</table>";
